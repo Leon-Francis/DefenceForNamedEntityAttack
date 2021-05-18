42bd91e0bb06a753e8c189361f0094d9ac1cfb3f
Leon-Francis <15290552788@163.com>
2021-05-18 19:48:23 +0800

    start training NE_attach inhance version

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,3 +1,3 @@
 currTime=$(date +"%Y-%m-%d_%T")
 
-nohup python ./baseline/baseline_train.py > ./baseline/logs/train_baseline_IMDB_Bert_attach_NE_${currTime}_0.log 2>&1 &
+nohup python ./baseline/baseline_train.py > ./baseline/logs/train_baseline_IMDB_Bert_attach_NE_inhance_${currTime}_0.log 2>&1 &
